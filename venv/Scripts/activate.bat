86e7851857808e6811efe01b142da914eadc573a
RQAmaral <rodrigo.queiroga.amaral@gmail.com>
2021-12-25 19:07:26 +0000

    Changed files locations

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\J\Desktop\assessments and uni doc\COM 526\ASSESSMENT\INTRODUCTION_TO_ARTIFICIAL_INTELLIGENCE_COM526_AE2\venv
+set VIRTUAL_ENV=C:\Users\J\Desktop\assessments and uni doc\COM 526\ASSESSMENT\INTRODUCTION_TO_ARTIFICIAL_INTELLIGENCE_COM526_AE2\assessment\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
