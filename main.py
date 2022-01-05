2d02a678085ba2810ed4731038527eb06a9660f7
RQAmaral <rodrigo.queiroga.amaral@gmail.com>
2022-01-05 05:26:54 +0000

    Added GUI

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 #importing libraries:
-from numpy import split
-import google_searcher2 as gr2
-import response_management as rm
-
-
-
+import window_render as wr
 
 #Starting the chat
-def main():
 
-    print('Welcome to the chatbot! Say Hello to start a conversation!')
-
-    exit_list = ['exit','see you later','bye']
-
-    while (True):
-
-        user_input = input('You: ')
-
-        if user_input.lower() in exit_list:
-            print('Bot: See you next time! :)')
-            break
-
-        elif user_input.lower().startswith('google search'):
-            print(gr2.google_search())
-
-        else:
-            if rm.check_all_messages(user_input) != None:
-                print('Bot: ' + rm.get_response(user_input))
-
-            else:
-               print('Bot: ' + rm.get_response(user_input))
-
-main()
+wr.logic()
--- window_render.py
+++ window_render.py
@@ -1,46 +1,53 @@
 from tkinter import *
+from numpy import split
+import google_searcher2 as gr2
+import response_management as rm
+import time
 
 window = Tk() #independent window
-window.title("ChatBot")
-window.geometry('500x300')
+window.title("CHATBOT")
+txt = Text(window)
+txt.grid(row=0, column=0, columnspan=2)
+e = Entry(window, width=100)
+e.grid(row = 1, column=0)
 
+def send():
+    user_input = e.get()
+    send = 'You: ' + e.get()
 
-label = Label(window, text='hola ninhooooo',fg='blue',font=('Arial',14))
-label.grid(row=0,column=0,padx=5,pady=10)
+    txt.insert(END, "\n" + send)
+    e.delete(0, END)
+    txt.see(END)
 
-data=StringVar()
+    return user_input
 
-textbox1=Entry(window,textvariable=data,fg='green',font=('Arial',14))
-textbox1.grid(row=0,column=1)
+#--------------------------------Logic---------------------------------------
 
-def myfunction():
-    emptylabel.config(text='hello')
+txt.insert(END,'Welcome to the chatbot! Say Hello to start a conversation!')
 
-button1=Button(window,command=myfunction,text='submit',fg='green',font=('Arial',14))
-button1.grid(row=1,column=1,sticky=W)
+exit_list = ['exit','see you later','bye']
 
-emptylabel = Label(window,fg='blue',font=('Arial',14))
-emptylabel.grid(row=2,column=1,sticky=W)
+def logic():
+        
+        user_input = send()
 
+        if user_input.lower() in exit_list:
+            txt.insert(END, "\n" +'Bot: See you next time! :)')
+            time.sleep(3)
+            window.destroy()
+
+        elif user_input.lower().startswith('google search'):
+            txt.insert(END, "\n" +'Bot: Type what you would like to search on the console!')
+            search = gr2.google_search()
+            txt.insert(END, "\n" + 'Bot: ' +  search)
+
+        else:
+            if rm.check_all_messages(user_input) != None:
+                txt.insert(END, "\n" +'Bot: ' + rm.get_response(user_input))
+
+            else:
+                txt.insert(END, "\n" +'Bot: ' + rm.get_response(user_input))
+
+#----------------------------------------------------------------------------
+send_button = Button(window, text='Send', command = logic).grid(row=1, column=1)
 window.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
